b5340c49807163601877abd9ea6efeb5f4665c63
palm000palmovich <ivankiselev6534@gmail.com>
2024-12-05 19:42:33 +0300

    start

---
+++ src/main/java/com/example/Library_DB/model/Book.java
@@ -0,0 +1,82 @@
+package com.example.Library_DB.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "Books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String author;
+    private String name;
+    private int year;
+
+    public Book(){}
+
+    public Book(String author, String name, int year) {
+        this.author = author;
+        this.name = name;
+        this.year = year;
+    }
+
+    //Геттеры и сеттеры
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(id, book.id) && Objects.equals(author, book.author) && Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, name, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
